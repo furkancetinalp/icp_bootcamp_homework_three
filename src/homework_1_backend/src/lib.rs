216dd400c9df1c84409b9b5f91b2aa63e8a09cf7
Furkan Cetinalp <cetinalp.furkan0@gmail.com>
2024-01-30 23:11:24 +0300

    first commit

--- src/homework_1_backend/src/lib.rs
+++ src/homework_1_backend/src/lib.rs
@@ -1,4 +1,136 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpMethod,
+};
+
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
+use serde::Serialize;
+use std::{borrow::Cow, cell::RefCell}; 
+
+
+//weather condition struct for deserializing response
+#[derive(CandidType, Deserialize, Clone)]
+#[derive(Default, Debug, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Condition {
+    pub id:f64,
+    pub main: String,
+    pub description: String,
+}
+
+//wind info struct for deserializing response
+#[derive(CandidType, Deserialize, Clone)]
+#[derive(Default, Debug, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Wind {
+    pub speed: f64,
+    pub deg: f64,
+}
+
+// main data struct for deserializing response
+#[derive(CandidType, Deserialize, Clone)]
+#[derive(Default, Debug, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Main {
+    pub temp: f64,
+    #[serde(rename = "feels_like")]
+    pub feels_like: f64,
+    #[serde(rename = "temp_min")]
+    pub temp_min: f64,
+    #[serde(rename = "temp_max")]
+    pub temp_max: f64,
+    pub pressure: f64,
+    pub humidity: f64,
+}
+
+//base struct
+#[derive(CandidType, Deserialize, Clone)]
+#[derive(Default, Debug, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Weather {
+    pub main: Main,
+    pub weather: Vec<Condition>,
+    pub wind:Wind
+
+}
+
+// Implementing Storable for Weather
+impl Storable for Weather {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+//memory definition
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+const MAX_VALUE_SIZE: u32 = 100;
+
+// Implementing BoundedStorable for Weather
+impl BoundedStorable for Weather {
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+// Creating memory manager with a new MemoryId
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+    RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static WEATHER_MAP: RefCell<StableBTreeMap<u64, Weather,Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1))), // Use a different MemoryId if needed
+        )
+    );
+}
+
+
+//getting weather data by city name
+#[ic_cdk::update]
+async fn get_weather_by_city_name(city_name: String) -> Result<Weather,String> {
+    let api_endpoint = "https://api.openweathermap.org/data/2.5/weather?q=";
+    let new_city_name = city_name.replace(" ", "%20");
+
+    //key value to access
+    let api_key = "a192e3072dae06165a3e7ec914bfa215";
+
+    // url to be sent
+    let url = format!("{}{}&units=metric&APPID={}", api_endpoint, new_city_name, api_key);
+
+    let request_headers = vec![];
+
+    // setting the request params,headers and body
+    let request = CanisterHttpRequestArgument {
+        url,
+        method: HttpMethod::GET,
+        body: None,
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+
+    //if Ok => deserializing response
+    //else => returning a string message
+    match http_request(request).await {
+        Ok((response,)) => {
+            if response.status == 200 {
+                let weather_data: Weather =
+                    serde_json::from_slice(&response.body).expect("Failed to parse JSON response.");
+                
+                return Ok(weather_data);
+            } else {
+                Err(format!("HTTP request failed with status code: {}", response.status))
+            }
+        }
+        Err((code, message)) => {
+            Err(format!(
+                "The http_request resulted in an error. Code: {:?}, Message: {}",
+                code, message
+            ))
+        }
+    }
 }
--- src/homework_1_frontend/src/index.js
+++ src/homework_1_frontend/src/index.js
@@ -1,19 +1,79 @@
 import { homework_1_backend } from "../../declarations/homework_1_backend";
+const date = new Date();
+
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+document.getElementById("datetime").innerText = `${hours}:${minutes} , ${day}-${month}-${year}`;
+
 
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
-
   const name = document.getElementById("name").value.toString();
 
   button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await homework_1_backend.greet(name);
-
+  get_weather_data(name);
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
 
   return false;
 });
+
+async function get_weather_data(name){
+  const api_data = await homework_1_backend.get_weather_by_city_name(name);
+  document.getElementById("city_name").innerText = name
+
+  // let obj = JSON.parse(JSON.stringify(api_data));
+  // let error = api_data["Err"];
+  let data = api_data["Ok"];
+  if (data == undefined) {
+    document.getElementById("temprature").innerText = "-"
+    document.getElementById("weather_condition").innerText = "-"
+    document.getElementById("humidity").innerText = "-"
+    document.getElementById("wind").innerText = "-"
+    document.getElementById("wind_direction").innerText = "-"
+
+  }
+  else {
+    console.log(data);
+    document.getElementById("temprature").innerText = `${data["main"].temp}\xB0C `
+    document.getElementById("weather_condition").innerText = data["weather"][0]["description"]
+    document.getElementById("humidity").innerText = data["main"].humidity;
+    document.getElementById("wind").innerText = `${data["wind"].speed} km/h`;
+    document.getElementById("wind_direction").innerHTML = `${data["wind"].deg}&deg;`
+
+    var weather_icon_id = data["weather"][0]["id"];
+    var weather_icon_result = Math.trunc(weather_icon_id/100);
+    if(weather_icon_id !=800 && weather_icon_result==8){
+      weather_icon_result="8x"
+    }
+    console.log(weather_icon_result);
+    document.getElementById("weather_icon").src = `${weather_icon_result}.png`
+  }
+
+  const date = new Date();
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  document.getElementById("datetime").innerText = `${hours}:${minutes} , ${day}-${month}-${year}`;
+
+}
+
+
+document.getElementById("london").onclick = function(){
+  get_weather_data("London")
+}
+document.getElementById("istanbul").onclick = function(){
+  get_weather_data("Istanbul")
+}
+document.getElementById("california").onclick = function(){
+  get_weather_data("California")
+}
+document.getElementById("sydney").onclick = function(){
+  get_weather_data("Sydney")
+}
